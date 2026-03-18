89f2438f9122cd5e31824b5974d63f7b7ae90b82
Sean Tietz <sean.tietz2@gmail.com>
2026-03-18 09:30:15 +0100

    Updated downloader to be crossplatform and safe to run multiple times

--- downloader.py
+++ downloader.py
@@ -1,13 +1,21 @@
 import requests
 import re
+from pathlib import Path
 
+DATA_DIR = "Wetterdaten"
 url = "https://opendata.dwd.de/climate_environment/CDC/observations_germany/climate/daily/kl/historical/"
+
+if Path(DATA_DIR).exists():
+    print("Wetterdaten Ordner existiert bereits, um Dateien erneut herunterzuladen, bitte Ordner löschen")
+    exit()
+
+Path(DATA_DIR).mkdir()
 
 entire_page = requests.get(url).text
 files = re.findall(r'href="([^"]+\.(?:zip|txt))"', entire_page)
 
 for file in files:
     data = requests.get(url + file).content
-    open("Wetterdaten/" + file, "wb").write(data)
+    open(DATA_DIR + "/" + file, "wb").write(data)
 
 print(f"Downloaded {len(files)} files!")
