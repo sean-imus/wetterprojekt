39f39b0fc89cbb1027bb6f7197d7a176403d011c
Sean Tietz <sean.tietz2@gmail.com>
2026-03-18 09:39:29 +0100

    Check if extracted files already exist as to not duplicate

--- extractor.py
+++ extractor.py
@@ -5,6 +5,12 @@ from pathlib import Path
 zip_folder = "Wetterdaten"
 output_folder = "Wetterdaten_extracted"
 
+if Path(output_folder).exists():
+    print("Ordner für extrahierte Wetterdaten existiert bereits, um Dateien erneut zu extrahieren, bitte Ordner löschen")
+    exit()
+
+Path(output_folder).mkdir()
+
 zip_files = Path(zip_folder).glob("*.zip")
 
 for zip_file in zip_files:
