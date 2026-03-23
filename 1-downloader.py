35cae5864624e776ab0bd7fecb63fa60256180e3
Sean Tietz <sean.tietz2@gmail.com>
2026-03-23 12:03:24 +0100

    rename variable

--- 1-downloader.py
+++ 1-downloader.py
@@ -2,21 +2,21 @@ import requests
 import re
 from pathlib import Path
 
-DATA_DIR = "Wetterdaten"
+output_folder = "Wetterdaten"
 url = "https://opendata.dwd.de/climate_environment/CDC/observations_germany/climate/daily/kl/historical/"
 
-if Path(DATA_DIR).exists():
-    print(f"{DATA_DIR} Ordner existiert bereits, um Dateien erneut herunterzuladen, bitte Ordner löschen")
+if Path(output_folder).exists():
+    print(f"{output_folder} Ordner existiert bereits, um Dateien erneut herunterzuladen, bitte Ordner löschen")
     exit()
 
-Path(DATA_DIR).mkdir()
+Path(output_folder).mkdir()
 
 entire_page = requests.get(url).text
 files = re.findall(r'href="([^"]+\.(?:zip|txt))"', entire_page)
 
 for file in files:
     data = requests.get(url + file).content
-    open(Path(DATA_DIR) / file, "wb").write(data)
+    open(Path(output_folder) / file, "wb").write(data)
     print(f"{file} heruntergeladen!")
 
 print(f"{len(files)} Dateien heruntergeladen!")
