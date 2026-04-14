ce3025370a7f3fcc742f9a48d83daaada41db67e
Sean Tietz <sean.tietz2@gmail.com>
2026-04-14 08:11:44 +0200

    added flake for setting up dev env

---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+  description = "Wetterprojekt development environment";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+  outputs = { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      devShells.${system}.default = pkgs.mkShellNoCC {
+        packages = with pkgs; [
+          python314
+          python314Packages.requests
+          python314Packages.tkinter
+          python314Packages.matplotlib
+          sqlite
+        ];
+      };
+    };
+}
