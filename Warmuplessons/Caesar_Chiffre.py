fdac52b7702358147ad88d870ff2c63d4305bb76
sean tietz <sean.tietz2@gmail.com>
2026-06-10 08:55:44 +0200

    some changes to make it compatible with tests

--- Warmuplessons/Caesar_Chiffre.py
+++ Warmuplessons/Caesar_Chiffre.py
@@ -1,19 +1,21 @@
-text = input("Geben Sie Ihren Text ein: ")
-offset = int(input("Um wieviel willst du deinen Text verschieben? "))
-
 def chiffre(text, offset):
     result = ""
     for char in text:
-        if 'a' <= char <= 'z':
-            result += chr((ord(char) - ord('a') + offset) % 26 + ord('a'))
-        elif 'A' <= char <= 'Z':
-            result += chr((ord(char) - ord('A') + offset) % 26 + ord('A'))
+        if "a" <= char <= "z":
+            result += chr((ord(char) - ord("a") + offset) % 26 + ord("a"))
+        elif "A" <= char <= "Z":
+            result += chr((ord(char) - ord("A") + offset) % 26 + ord("A"))
         else:
             result += char
     return result
 
-encrypted = chiffre(text, offset)
-print("Verschlüsselt:", encrypted)
 
-decrypted = chiffre(encrypted, -offset)
-print("Entschlüsselt:", decrypted)
+if __name__ == "__main__":
+    text = input("Geben Sie Ihren Text ein: ")
+    offset = int(input("Um wieviel willst du deinen Text verschieben? "))
+
+    encrypted = chiffre(text, offset)
+    print("Verschlüsselt:", encrypted)
+
+    decrypted = chiffre(encrypted, -offset)
+    print("Entschlüsselt:", decrypted)
