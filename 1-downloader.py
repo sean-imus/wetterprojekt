c6533e1c5f02535e40c4359cc4c480a7205ebf73
sean tietz <sean.tietz2@gmail.com>
2026-06-10 10:20:25 +0200

    lsp formatting

--- 1-downloader.py
+++ 1-downloader.py
@@ -6,29 +6,21 @@ output_dir = "Wetterdaten"
 url = "https://opendata.dwd.de/climate_environment/CDC/observations_germany/climate/daily/kl/historical/"
 
 if Path(output_dir).exists():
-    print(f"{output_dir} Ordner existiert bereits, um Dateien erneut herunterzuladen, bitte Ordner löschen")
+    print(
+        f"{output_dir} Ordner existiert bereits, um Dateien erneut herunterzuladen, bitte Ordner löschen"
+    )
     exit()
 
 Path(output_dir).mkdir()
 
+print("Seite wird abgerufen...")
+seite = requests.get(url, timeout=30).text
+dateien = re.findall(r'href="([^"]+(?:zip|txt))"', seite)
+
 print("Dateien werden heruntergeladen...")
-
-try:
-    entire_page = requests.get(url, timeout=30).text
-except requests.RequestException as e:
-    print(f"Fehler beim Abrufen der Seite: {e}")
-    exit()
-
-files = re.findall(r'href="([^"]+(?:zip|txt))"', entire_page)
-
-for file in files:
-    try:
-        response = requests.get(url + file, timeout=60)
-        response.raise_for_status()
-        with open(Path(output_dir) / file, "wb") as f:
-            f.write(response.content)
-    except requests.RequestException as e:
-        print(f"Fehler beim Herunterladen von {file}: {e}")
-        continue
+for datei in dateien:
+    antwort = requests.get(url + datei, timeout=60)
+    with open(Path(output_dir) / datei, "wb") as f:
+        f.write(antwort.content)
 
 print("Dateien heruntergeladen!")
